9d823e44f74a53e08c71d540d21fed61590d5ea8
Osée Masuaku <omasuaku@gmail.com>
2025-12-24 02:33:15 +0100

    Remove play_integrity_test and test_agent files

--- test_agent.py
+++
@@ -1,9 +0,0 @@
-from agent import agent
-
-query = "Explique-moi l'article 41 s'il te plaît?"
-
-for event in agent.stream(
-    {"messages": [{"role": "user", "content": query}]},
-    stream_mode="values",
-):
-    event["messages"][-1].pretty_print()
