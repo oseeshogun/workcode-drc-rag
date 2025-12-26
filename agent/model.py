ba6ca98315e59112511eb79abcb4d2616d56cc1e
Osée Masuaku <omasuaku@gmail.com>
2025-12-26 15:28:33 +0100

    Change model to gemini-2.5-pro

--- agent/model.py
+++ agent/model.py
@@ -3,4 +3,4 @@ from langchain.chat_models import init_chat_model
 
 load_dotenv()
 
-model = init_chat_model("google_genai:gemini-2.5-flash-lite")
+model = init_chat_model("google_genai:gemini-2.5-pro")
